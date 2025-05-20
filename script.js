4fadba63636345bd552f382c2f4212f8f204e2dc
Arindam2003 <dindaarindam000@gmail.com>
2025-05-20 20:57:16 +0530

    3 completed

---
+++ script.js
@@ -0,0 +1,147 @@
+//! Open and Back Feature...
+function OpenBack() {
+    var allElem = document.querySelectorAll(".elem");
+    var fullElemPage = document.querySelectorAll(".fullElem");
+    var fullElemPageBack = document.querySelectorAll(".fullElem .back");
+
+    //! For Open the Page
+
+    allElem.forEach(function (elem) {
+        elem.addEventListener('click', function () {
+
+            fullElemPage[elem.id].style.display = 'block';
+        })
+    })
+
+    //! For Back
+
+    fullElemPageBack.forEach(function (back) {
+        back.addEventListener('click', function () {
+            fullElemPage[back.id].style.display = 'none';
+        })
+
+    })
+}
+OpenBack()
+
+//! To Do List Completed...
+function ToDoList() {
+
+
+    var currentTask = []
+
+    if (localStorage.getItem('currentTask')) {
+        currentTask = JSON.parse(localStorage.getItem('currentTask'))
+    }
+
+    function renderTask() {
+        var allTask = document.querySelector(".allTask");
+        sum = ''
+        currentTask.forEach(function (elem, idx) {
+            sum = sum + `<div class="task">
+                <h5>${elem.task} <span class=${elem.imp}>imp</span></h5>
+            <button id=${idx}>Mark as Completed</button>
+                </div>`
+        })
+        allTask.innerHTML = sum
+
+        //! akhn e dhukanor karon hoche amk ektadlt korar por abr to button eracess pete hobe tai mne 1ta dlt thik aceh then abr sob dislay te render houar sathe sathe abr acess paor jonno
+        var markCompleted = document.querySelectorAll('.task button');
+        markCompleted.forEach(function (btn) {
+            btn.addEventListener('click', function () {
+                currentTask.splice(btn.id, 1);
+                localStorage.setItem("currentTask", JSON.stringify(currentTask));
+                // console.log(currentTask);
+                renderTask();
+            })
+        })
+    }
+    renderTask()
+
+    var form = document.querySelector(".addTask form");
+    var taskInput = document.querySelector(".addTask form input");
+    var taskDetails = document.querySelector(".addTask form textarea");
+    var taskCheckBox = document.querySelector(".addTask form #mark");
+
+
+    form.addEventListener('submit', function (dets) {
+        dets.preventDefault();
+        currentTask.push(
+            {
+                task: taskInput.value,
+                details: taskDetails.value,
+                imp: taskCheckBox.checked
+            }
+        )
+        localStorage.setItem('currentTask', JSON.stringify(currentTask));
+
+        renderTask();
+        taskInput.value = '';
+        taskDetails.value = '';
+        taskCheckBox.checked = false;
+    })
+}
+ToDoList();
+
+//! Daily Planner...
+
+function DailyPlanner()
+{
+    var dailyPlanner = document.querySelector(".day-planner");
+
+    var dayPlanData = JSON.parse(localStorage.getItem('dayPlanData')) || {}
+
+    var hour = Array.from({ length: 18 }, (_, idx) => `${6 + idx}:00 - ${7 + idx}:00`)
+
+
+    var wholeDay = ''
+
+
+    hour.forEach((elem, idx) => {
+
+        var saved = dayPlanData[idx] || ''
+
+        wholeDay = wholeDay + `<div class="day-planner-time">
+                    <p>${elem}</p>
+                    <input id=${idx} type="text" placeholder="..." value=${saved}>
+                </div>`;
+    })
+
+
+    dailyPlanner.innerHTML = wholeDay;
+
+
+    var dayPlannerInput = document.querySelectorAll(".day-planner input");
+
+    dayPlannerInput.forEach((elem) => {
+        elem.addEventListener('input', () => {
+            dayPlanData[elem.id] = elem.value;
+            localStorage.setItem('dayPlanData', JSON.stringify(dayPlanData));
+        })
+    })
+}
+DailyPlanner();
+
+
+//! Motivational Quote...
+
+function Motivational()
+{
+    var motivationQuote = document.querySelector(".motivation-2 h1");
+    var motivationAuthor = document.querySelector(".motivation-3 h3");
+    // window.location.reload();
+    async function fetchQuote() {
+        let response = await fetch("https://api.quotable.io/random")
+        let data = await response.json();
+        motivationQuote.innerHTML = `" ${data.content} "`;
+        motivationAuthor.innerHTML = `- ${data.author}`
+    }
+
+    fetchQuote();
+    let motiBack = document.querySelector(".motiback");
+    motiBack.addEventListener("click", () => {
+        window.location.reload();
+    })
+}
+Motivational()
+
